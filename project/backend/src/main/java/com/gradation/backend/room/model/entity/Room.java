8d169ff308054844115d356ef88072fa123bcc6c
박종경 <gd122572@gmail.com>
2025-01-24 10:56:54 +0900

    feat: config 설정

---
+++ project/backend/src/main/java/com/gradation/backend/room/model/entity/Room.java
@@ -0,0 +1,30 @@
+package com.gradation.backend.room.model.entity;
+
+import com.gradation.backend.user.model.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "Room")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100)
+    private String title;
+
+    @Column
+    private Integer password;
+
+    @Column(name = "game_status", nullable = false)
+    private Boolean gameStatus;
+
+    @OneToOne
+    @JoinColumn(name = "host_id", foreignKey = @ForeignKey(name = "FK_user_name"), nullable = false)
+    private User host;
+}
