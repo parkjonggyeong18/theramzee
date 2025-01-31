da9d07d3d7e348cbaacfb90d07d5d114b282fc0b
purekim333 <purekim333@gmail.com>
2025-01-31 16:03:30 +0900

    feat: 오픈비두 예외처리 추가

---
+++ project/backend/src/main/java/com/gradation/backend/openvidu/exception/OpenViduExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.gradation.backend.openvidu.exception;
+
+import com.gradation.backend.common.model.response.BaseResponse;
+import io.openvidu.java.client.OpenViduHttpException;
+import io.openvidu.java.client.OpenViduJavaClientException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class OpenViduExceptionHandler {
+
+    // 오픈비두 클라이언트 에러
+    @ExceptionHandler(OpenViduJavaClientException.class)
+    public ResponseEntity<BaseResponse<String>> handleOpenViduJavaClientException(OpenViduJavaClientException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(BaseResponse.error("OpenVidu Client Exception: " + e.getMessage()));
+    }
+
+    // 오픈비두 HTTP 에러
+    @ExceptionHandler(OpenViduHttpException.class)
+    public ResponseEntity<BaseResponse<String>> handleOpenViduHttpException(OpenViduHttpException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(BaseResponse.error("OpenVidu HTTP Exception: " + e.getMessage()));
+    }
+}
