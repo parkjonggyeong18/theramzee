56e40df6cab66f49cb7b350d109bd6c8ffbf64a6
김경환 <k2sc2000@gmail.com>
2025-02-03 12:46:38 +0900

    fix: hotfix

--- project/backend/src/main/java/com/gradation/backend/room/model/request/JoinRoomRequest.java
+++ project/backend/src/main/java/com/gradation/backend/room/model/request/JoinRoomRequest.java
@@ -4,9 +4,5 @@ import lombok.Data;
 
 @Data
 public class JoinRoomRequest {
-<<<<<<< HEAD
-//    private String nickname;
-=======
->>>>>>> 7bedab95993718149e3ad38c094d34e57a2dcb1c
     private Integer password;
 }
