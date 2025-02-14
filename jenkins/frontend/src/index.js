e02767ade187d94a49854c9bafb1f6f79bef7888
MurHyun2 <eddy152264@gmail.com>
2025-02-15 00:01:33 +0900

    fix: docker-compose.yml frontend & nginx volumes mount

--- jenkins/frontend/src/index.js
+++ jenkins/frontend/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log('App version:', process.env.REACT_APP_VERSION);
 root.render(
   //<React.StrictMode>
     <App />
