c020bc4362533173de0c99fe040b4ee29106602e
이대현 <eddy1219@naver.com>
2025-02-14 14:42:50 +0900

    fix: nginx.conf fixed

--- jenkins/backend/src/main/java/com/gradation/backend/common/config/SecurityConfig.java
+++ jenkins/backend/src/main/java/com/gradation/backend/common/config/SecurityConfig.java
@@ -95,7 +95,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 // URL 접근 권한 설정
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/**", "/swagger-ui/**", "/v3/api-docs/**","/api-docs/**","/ws/**", "/user/**","/swagger-ui.html","/game-socket").permitAll() // 인증 없이 접근 가능 경로
+                        .requestMatchers("https://ramzee.online/api/v1/**","/api/v1/**", "/swagger-ui/**", "/v3/api-docs/**","/api-docs/**","/ws/**", "/user/**","/swagger-ui.html","/game-socket","/wss/**").permitAll() // 인증 없이 접근 가능 경로
                         .anyRequest().authenticated() // 나머지 요청은 인증 필요
                 )
                 // JWT 인증 필터를 UsernamePasswordAuthenticationFilter 앞에 추가
