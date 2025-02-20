aba95a3ae45a13eb336f1da6324366a252b3a7f4
이대현 <eddy1219@naver.com>
2025-02-21 07:03:07 +0900

    Deleted 김경환/AI/MediaPipe.html, 김경환/AI/MediaPipe2.html, 김경환/AI/PoseNet.html, 김경환/AI/face_API.html, 김경환/AI/face_API2.html, 김경환/AI/output_video.avi, 김경환/AI/test.MOV, 김경환/AI/web_speech_API.html, 김경환/AI/모션인식test.py, 김경환/README.md, 김선진/readme.md, 김승윤/README.md, 박종경/demo1/gradle/wrapper/gradle-wrapper.jar, 박종경/demo1/gradle/wrapper/gradle-wrapper.properties, 박종경/demo1/src/main/java/com/example/demo/common/config/RedisConfig.java, 박종경/demo1/src/main/java/com/example/demo/common/config/SwaggerConfig.java, 박종경/demo1/src/main/java/com/example/demo/common/model/response/BaseResponse.java, 박종경/demo1/src/main/java/com/example/demo/common/util/RedisUtil.java, 박종경/demo1/src/main/java/com/example/demo/user/config/EmailConfig.java, 박종경/demo1/src/main/java/com/example/demo/user/config/PasswordEncoderConfig.java, 박종경/demo1/src/main/java/com/example/demo/user/config/SecurityConfig.java, 박종경/demo1/src/main/java/com/example/demo/user/controller/AuthController.java, 박종경/demo1/src/main/java/com/example/demo/user/controller/EmailController.java, 박종경/demo1/src/main/java/com/example/demo/user/controller/UserController.java, 박종경/demo1/src/main/java/com/example/demo/user/exception/UserNotFoundException.java, 박종경/demo1/src/main/java/com/example/demo/user/filter/JwtAuthenticationFilter.java, 박종경/demo1/src/main/java/com/example/demo/user/model/dto/EmailCheckDto.java, 박종경/demo1/src/main/java/com/example/demo/user/model/entity/User.java, 박종경/demo1/src/main/java/com/example/demo/user/model/request/EmailRequest.java, 박종경/demo1/src/main/java/com/example/demo/user/model/request/UserRequest.java, 박종경/demo1/src/main/java/com/example/demo/user/model/response/UserResponse.java, 박종경/demo1/src/main/java/com/example/demo/user/repository/UserRepository.java, 박종경/demo1/src/main/java/com/example/demo/user/service/impl/EmailServiceImpl.java, 박종경/demo1/src/main/java/com/example/demo/user/service/impl/UserServiceImpl.java, 박종경/demo1/src/main/java/com/example/demo/user/service/EmailService.java, 박종경/demo1/src/main/java/com/example/demo/user/service/UserService.java, 박종경/demo1/src/main/java/com/example/demo/user/util/JwtTokenUtil.java, 박종경/demo1/src/main/java/com/example/demo/DemoApplication.java, 박종경/demo1/src/main/resources/application.yaml, 박종경/demo1/src/test/java/com/example/demo/DemoApplicationTests.java, 박종경/demo1/.gitattributes, 박종경/demo1/.gitignore, 박종경/demo1/README.md, 박종경/demo1/build.gradle, 박종경/demo1/gradlew, 박종경/demo1/gradlew.bat, 박종경/demo1/settings.gradle, 신경원/.gitkeep, 신경원/readme.md

--- 박종경/demo1/src/main/java/com/example/demo/user/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.user.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
